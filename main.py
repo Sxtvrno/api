0b1d02677a75fd798d915ad2ff33ac834ae0b956
Sxtvrno <fr.vergarah@duocuc.cl>
2024-05-14 04:11:09 -0400

    faltó solo el put

--- main.py
+++ main.py
@@ -1,37 +1,43 @@
 import pymysql
-import pymysql.cursors
 from app import app
 from config import mysql
-from flask import jsonify
-from flask import request
+from flask import jsonify, request, redirect
 
 
-@app.route('/create/<id_producto>/<nombre_producto>/<valor_producto>/', methods=['POST'])
-def create_producto(id_producto, nombre_producto, valor_producto):
+
+@app.route('/')
+def inicio():
+    return("HOLA MUNDO")
+
+
+
+@app.route('/create/<id_producto2>/<nombre_producto2>/<valor_producto2>/<tipo_producto2>', methods=['POST'])
+def create_producto(id_producto2, nombre_producto2, valor_producto2, tipo_producto2):
     try:
-        conn = mysql.connect
-        cursor = conn.cursor()
-        sqlQuery = "INSERT INTO producto(id_producto, nombre_producto, valor_producto) VALUES(DEFAULT, %s, %s, %s)"
-        bindData = (id_producto, nombre_producto, valor_producto)
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        sqlQuery = "INSERT INTO producto(id_producto, nombre_producto, valor_producto, tipo_producto) VALUES(%s, %s, %s,%s)"
+        bindData = (id_producto2, nombre_producto2, valor_producto2, tipo_producto2)
         cursor.execute(sqlQuery, bindData)
         conn.commit()
         response = jsonify('Producto agregado exitosamente')
         response.status_code = 200
-        cursor.close()
-        conn.close()
         return response
     except Exception as e:
         print(e)
+    finally:
+        cursor.close() 
+        conn.close() 
 
 
 
-@app.route('/', methods=['GET'])
+@app.route('/productos', methods=['GET'])
 def info_prod():
         try:
               
             conn = mysql.connect()
             cursor = conn.cursor(pymysql.cursors.DictCursor)
-            cursor.execute("SELECT id_producto, nombre_producto, valor_producto FROM producto")
+            cursor.execute("SELECT id_producto, tipo_producto, nombre_producto, valor_producto  FROM producto")
             empRows = cursor.fetchall()
             response = jsonify(empRows)
             response.status_code = 200
@@ -65,12 +71,12 @@ def detalle_prod(id_producto):
 def actualizar_prod():
     try:
         _json = request.json
-        _id = _json['id_producto']
-        _name = _json['nombre_producto']
-        _price = _json['valor_producto']
-        if _name and _price and _id and request.method == 'PUT':			
-            sqlQuery = "UPDATE producto SET nombre_producto=%s, precio=%s WHERE id_producto=%s"
-            bindData = (_name, _price, _id,)
+        _id_producto = _json['id_producto']
+        _nombre_producto = _json['nombre_producto']
+        _valor_producto = _json['valor_producto']
+        if _id_producto and _nombre_producto and _valor_producto and request.method == 'PUT':			
+            sqlQuery = "UPDATE producto SET nombre_producto=%s, valor_producto=%s WHERE id_producto=%s"
+            bindData = (_nombre_producto, _valor_producto, _id_producto,)
             conn = mysql.connect()
             cursor = conn.cursor()
             cursor.execute(sqlQuery, bindData)
@@ -89,19 +95,21 @@ def actualizar_prod():
 
 
 @app.route('/delete/<int:id_producto>', methods=['DELETE'])
-def eliminar_prod(idProducto):
+def eliminar_prod(id_producto):
     try:
-        conn = mysql.connect
+        conn = mysql.connect()
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM producto WHERE id_producto =%s", {idProducto})
+        cursor.execute("DELETE FROM producto WHERE id_producto =%s", (id_producto,))
         conn.commit()
-        response = jsonify("Producto de código %s borrado" % idProducto)
+        response = jsonify("Producto de código %s borrado" % id_producto)
         response.status_code = 200
-        cursor.close()
-        conn.close()
         return response
     except Exception as e:
         print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
 
 
 
